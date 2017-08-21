b640ff45d623f4e9609f0e4c3a4b825a825653cc
Michelle Sri <michelle.sri@gmail.com>
2017-08-21 15:35:25 -0700

    require querystring fs and port

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const url = require('url');
+const querystring = require('querystring');
+const fs = require('fs');
+const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   console.log('req.url: ', req.url);
