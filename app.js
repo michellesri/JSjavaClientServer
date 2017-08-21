3bc20067c85f01c26726c6afb078642242b89535
Michelle Sri <michelle.sri@gmail.com>
2017-08-21 15:34:59 -0700

    parse the params from querystring

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ const url = require('url');
 const server = http.createServer((req, res) => {
   console.log('req.url: ', req.url);
   var myUrl = url.parse(req.url);
+  var params = querystring.parse(myUrl.query);
